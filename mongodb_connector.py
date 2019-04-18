f81351c41ce96fe5c331b0faf7266ede02df1b3a
Sahil Shah <sahilshah13496@gmail.com>
2019-04-18 00:05:40 -0400

    Added service to connect DB

---
+++ mongodb_connector.py
@@ -0,0 +1,44 @@
+import pymongo
+
+class dbHandler:
+
+    # connect to database
+    def __init__(self):
+        print "Connecting to database..."
+        client = pymongo.MongoClient("mongodb+srv://SSG:Pipeline123@cluster0-rjugd.mongodb.net/test?retryWrites=true")
+        db = client.get_database("user_stats")
+        self.client = client
+        self.db = db
+        self.sentence_collection = self.db.get_collection("sentences")
+        self.word_collection = self.db.get_collection("words")
+        print "Database successfully connected!"
+
+    # need to reconnect to db for some reason (example lost connection)
+    def connectToDb(self):
+        client = pymongo.MongoClient("mongodb+srv://SSG:Pipeline123@cluster0-rjugd.mongodb.net/test?retryWrites=true")
+        db = client.get_database("user_stats")
+        self.client = client
+        self.db = db
+        self.sentence_collection = self.db.get_collection("sentences")
+        self.word_collection = self.db.get_collection("words")
+
+    # expects sentence in json format
+    def insertSentenceToDb(self, sentence):
+        try:
+            print "Inserting sentence to db..."
+            self.sentence_collection.insert_one(sentence)
+            print "Sentence inserted!"
+        except Exception as e:
+            print "Error in inserting sentence - ", e
+
+    def closeConnection(self):
+        self.client.close()
+
+test_json = {
+    "custom_id": 1,
+    "sentence": "This is a test sentence"
+}
+
+# dbHandlerService = dbHandler()
+# dbHandlerService.insertSentenceToDb(test_json)
+# dbHandlerService.closeConnection()
--- ssg.py
+++ ssg.py
@@ -2,8 +2,14 @@ from tornado.web import Application
 from tornado.web import RequestHandler
 from tornado.ioloop import IOLoop
 import json
+from mongodb_connector import dbHandler
 
 sentences = []
+class InputApplication(Application):
+    def __init__(self, handler_mapping):
+        self.db = dbHandler()
+        super(InputApplication, self).__init__(handler_mapping)
+
 class InputHandler(RequestHandler):
     def set_default_headers(self):
         super(InputHandler, self).set_default_headers()
@@ -12,13 +18,16 @@ class InputHandler(RequestHandler):
 
     def post(self):
         sentences.append(self.request.body)
-        self.write({'sentence': json.loads(self.request.body)})
+        self.write(json.loads(self.request.body))
+
+        # give input in dict/json format
+        self.application.db.insertSentenceToDb(json.loads(self.request.body))
 
 if __name__ == "__main__":
     handler_mapping = [
                        (r'/sentence', InputHandler),
                        (r'/sentence/', InputHandler),
                       ]
-    application = Application(handler_mapping)
+    application = InputApplication(handler_mapping)
     application.listen(7777)
     IOLoop.current().start()
